d47e420f2bcf240667961a50305953cbf8b0e2ad
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-23 19:44:48 -0300

    Fix: Requirement 1 - /products & /products/:id test bugs fixed

--- test/unit/models/ProductsModel.test.js
+++ test/unit/models/ProductsModel.test.js
@@ -1,52 +1,48 @@
 const sinon = require('sinon');
-const chai = require('chai');
+const { expect } = require('chai');
 const connection = require('../../../models/connection');
 const ProductsModel = require('../../../models/ProductsModel');
 const { products } = require('./mocks/StoreManagerMock');
 
-describe('Listar os Produtos', () => {
+describe('Listar todos os Produtos acessando o caminho "/products" e ...', () => {
   before( async () => {
-    sinon.stub(connection, 'execute').resolves(products);
+    sinon.stub(connection, 'execute').resolves([products]);
   });
 
   after( async () => {
     connection.execute.restore();
   });
 
-  describe('Se acessado o caminho "/products" deve...', () => {
-    it('não deve ser vazio.', async () => {
-      const response = await ProductsModel.getAll();
-      expect(response).to.be.not.empty;
-    });
-
-    it('retornar um array.', async () => {
+  describe('renornar ...', () => {
+    it('um objeto do tipo "array", ...', async () => {
       const response = await ProductsModel.getAll();
       expect(response).to.be.an('array');
     });
 
-    it('retornar um array com todos os produtos.', async () => {
+    it('não vazio e ...', async () => {
+      const response = await ProductsModel.getAll();
+      expect(response).to.be.not.empty;
+    });
+
+    it('com todos os produtos.', async () => {
       const response =  await ProductsModel.getAll();
-      expect(response).to.be.equal(products);
+      expect(response).to.be.eql(products);
     });
   });
+});
 
-  describe('Se acessado o caminho "/products/:id" deve...', () => {
-    before( async () => {
-        sinon.stub(connection, 'execute').resolves(products[0]);
-      });
-    
-      after( async () => {
-        connection.execute.restore();
-      });
+describe('Listar um Produto acessando o caminho "/products/:id" e ...', () => {
+  it('retornar um array vazio se o "id" não for encontrado e ...', async () => {
+    sinon.stub(connection, 'execute').resolves([[]]);
+    const response =  await ProductsModel.find(2);
+    expect(response).to.be.empty;
+    connection.execute.restore();
+  });
 
-    it('retornar um array vazio se o "id" não for encontrado.', async () => {
-      const response =  await ProductsModel.find(2);
-      expect(response).to.be.empty;
-    });
-
-    it('retorne o produto se o "id" estiver correto.', async () => {
-      const response =  await ProductsModel.find(1);
-      expect(response).to.be.equal(products[0]);
-    });
+  it('retornar o produto se o "id" for encontrado.', async () => {
+    sinon.stub(connection, 'execute').resolves([products[0]]);
+    const response =  await ProductsModel.find(1);
+    expect(response).to.be.eql(products[0]);
+    connection.execute.restore();
   });
 });
