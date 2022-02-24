c4e7d60f0ed6d959a687f4fae18256e95d08d202
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-24 07:40:03 -0300

    Refactor: Requirement 1 - /products & /products/:id test refactoring

--- test/unit/models/ProductsModel.test.js
+++ test/unit/models/ProductsModel.test.js
@@ -4,7 +4,7 @@ const connection = require('../../../models/connection');
 const ProductsModel = require('../../../models/ProductsModel');
 const { products } = require('./mocks/StoreManagerMock');
 
-describe('Listar todos os Produtos acessando o caminho "/products" e ...', () => {
+describe('Acessando o caminho "/products" ...', () => {
   before( async () => {
     sinon.stub(connection, 'execute').resolves([products]);
   });
@@ -13,36 +13,31 @@ describe('Listar todos os Produtos acessando o caminho "/products" e ...', () =>
     connection.execute.restore();
   });
 
-  describe('renornar ...', () => {
-    it('um objeto do tipo "array", ...', async () => {
+  describe('renorna ...', () => {
+    it('um objeto do tipo "array", e ...', async () => {
       const response = await ProductsModel.getAll();
       expect(response).to.be.an('array');
     });
 
-    it('não vazio e ...', async () => {
+    it('não vazio.', async () => {
       const response = await ProductsModel.getAll();
       expect(response).to.be.not.empty;
-    });
-
-    it('com todos os produtos.', async () => {
-      const response =  await ProductsModel.getAll();
-      expect(response).to.be.eql(products);
     });
   });
 });
 
-describe('Listar um Produto acessando o caminho "/products/:id" e ...', () => {
-  it('retornar um array vazio se o "id" não for encontrado e ...', async () => {
+describe('Acessando o caminho "/products/:id" ...', () => {
+  it('não retorna produto se o "id" não for encontrado e ...', async () => {
     sinon.stub(connection, 'execute').resolves([[]]);
     const response =  await ProductsModel.find(2);
     expect(response).to.be.empty;
     connection.execute.restore();
   });
 
-  it('retornar o produto se o "id" for encontrado.', async () => {
+  it('retornar um produto se o "id" for encontrado.', async () => {
     sinon.stub(connection, 'execute').resolves([products[0]]);
     const response =  await ProductsModel.find(1);
-    expect(response).to.be.eql(products[0]);
+    expect(response).to.be.not.empty;
     connection.execute.restore();
   });
 });
