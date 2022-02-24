8a9a410bfba27e72b3f5b0316bc013975560df02
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-24 19:15:52 -0300

    Fix: Requirement 2 - ProductsModel.js - SQLs lines fixed

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [result] = await connection.execute('SELECT * FROM products');
+  const [result] = await connection.execute('SELECT * FROM StoreManager.products');
 
   return result;
 };
 
 const find = async (id) => {
   const [result] = await connection.execute(
-    'SELECT * FROM products WHERE id = ?',
+    'SELECT * FROM StoreManager.products WHERE id = ?',
     [id],
   );
 
