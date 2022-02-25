b68a8b0353fecd3218d03bf1298e8c7f8d43fee1
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-25 15:54:17 -0300

    Feat: Requirement 4 - POST /products - Create method included

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ProductsController = require('./controllers/ProductsController');
 const SalesController = require('./controllers/SalesController');
+const { 
+  nameProductValidator,
+  quantityProductsValidator,
+} = require('./middlewares/ProductsMiddleware');
+const { 
+  productIdSalesValidator,
+  quantitySalesValidator,
+} = require('./middlewares/SalesMiddleware');
 require('dotenv').config();
 
 const app = express();
@@ -19,6 +27,15 @@ app.get('/products/:id', ProductsController.find);
 app.get('/sales', SalesController.getAll);
 
 app.get('/sales/:id', SalesController.find);
+
+app.post(
+  '/products',
+  nameProductValidator,
+  quantityProductsValidator,
+  ProductsController.create,
+);
+
+app.post('/sales', productIdSalesValidator, quantitySalesValidator);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
