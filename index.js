6e11673214594e50b82e72c92c141a5a096a910f
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-25 06:57:15 -0300

    Feat: Requirement 2 - Index.js file changed

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const ProductsController = require('./controllers/ProductsController');
 require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/products', ProductsController.getAll);
+
+app.get('/products/:id', ProductsController.find);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
