8e8512a6bbedfc173c1138625df77a0573747c73
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-25 06:55:46 -0300

    Add: Requirement 2 - ProductsController.js file included

---
+++ controllers/ProductsController.js
@@ -0,0 +1,30 @@
+const ProductsService = require('../services/ProductsService');
+
+const getAll = async (_req, res) => {
+  try {
+    const products = await ProductsService.getAll();
+    return res.status(200).json(products);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Internal Server Error');
+  }
+};
+
+const find = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await ProductsService.find(id);
+    if (product.length === 0) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    return res.status(200).json(product);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Internal Server Error');
+  }
+};
+
+module.exports = {
+  getAll,
+  find,
+};
