e17c36b7087694f8290927db8a077e8530be76a6
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-25 20:11:44 -0300

    Feat: Requirement 7 - SalesController.js - Create method included

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -1,12 +1,15 @@
+const error500 = { message: 'Internal Server Error' };
+
 const SalesService = require('../services/SalesService');
 
 const getAll = async (_req, res) => {
   try {
     const sales = await SalesService.getAll();
+
     return res.status(200).json(sales);
   } catch (err) {
     console.error(err);
-    return res.status(500).send('Internal Server Error');
+    return res.status(500).json(error500);
   }
 };
 
@@ -14,17 +17,36 @@ const find = async (req, res) => {
   const { id } = req.params;
   try {
     const sale = await SalesService.find(id);
+
     if (sale.length === 0) {
       return res.status(404).json({ message: 'Sale not found' });
     }
+
     return res.status(200).json(sale);
   } catch (err) {
     console.error(err);
-    return res.status(500).send('Internal Server Error');
+    return res.status(500).json(error500);
+  }
+};
+
+const create = async (req, res) => {
+  const arraySales = req.body;
+  try {
+    const sale = await SalesService.create(arraySales);
+
+    if (sale.length === 0) {
+      return res.status(402).json({ message: 'Payment required' });
+    }
+
+    return res.status(201).json(sale);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(error500);
   }
 };
 
 module.exports = {
   getAll,
   find,
+  create,
 };
