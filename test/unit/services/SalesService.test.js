132e5de98141b8f5516d6c8eef353c16dad64e82
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-25 07:53:09 -0300

    Add: Requirement 1 - /sales & /sales/:id - SalesService file included

---
+++ test/unit/services/SalesService.test.js
@@ -0,0 +1,55 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const SalesModel = require('../../../models/SalesModel');
+const SalesService = require('../../../services/SalesService');
+const { sales } = require('../mocks/StoreManagerMock');
+
+
+describe('Ao acessar o caminho', () => {
+  describe('GET "/sales" retorna', () => {
+    before( async () => {
+      sinon.stub(SalesModel, 'getAll').resolves(sales);
+    });
+
+    after( async () => {
+      SalesModel.getAll.restore();
+    });
+
+    it('a quantidade correta de vendas', async () => {
+      const response = await SalesService.getAll();
+      expect(response).to.be.length(2);
+    });
+
+    it('um array com as vendas corretas', async () => {
+      const response = await SalesService.getAll();
+      expect(response[0].productId).to.be.eql(sales[0].productId);
+      expect(response[1].productId).to.be.eql(sales[1].productId);
+    });
+
+    it('ordenados por "id" de forma crescente.', async () => {
+      const response = await SalesService.getAll();sales
+      expect(response[0].saleId).to.be.eql(sales[0].saleId);
+      expect(response[1].saleId).to.be.eql(sales[1].saleId);
+    });
+  });
+
+  describe('GET "/sales/:id" retorna', () => {
+    before( async () => {
+        sinon.stub(SalesModel, 'find').resolves(sales[0]);
+    });
+
+    after( async () => {
+        SalesModel.find.restore();
+    });
+
+    it('a quantidade correta de vendas', async () => {
+      const response =  await SalesService.find(1);
+      expect([response]).to.be.length(1);
+    });
+
+    it('a venda correta para o "id" informado.', async () => {
+      const response =  await SalesService.find(1);
+      expect(response).to.be.eql(sales[0]);
+    });
+  });
+});
