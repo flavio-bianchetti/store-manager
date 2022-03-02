43fb615d13bc0c9cad91e03ce9ab2fceb7e66a5e
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-02 17:52:27 -0300

    Add: Requirements 11 & 12 - added validations

--- services/SalesService.js
+++ services/SalesService.js
@@ -1,4 +1,48 @@
 const SalesModel = require('../models/SalesModel');
+const ProductsModel = require('../models/ProductsModel');
+
+const updateQuantityProducts = async (arraySales) => {
+  const products = await ProductsModel.getAll();
+  const resultProducts = await arraySales
+    .map(async (sale) => {
+      const product = products.find((p) => p.id === sale.productId);
+      const updateProduct = {
+        id: product.id,
+        name: product.name,
+        quantity: product.quantity - sale.quantity,
+      };
+      const result = await ProductsModel.update(updateProduct);
+      if (result.affectedRows === 0) return false;
+
+      return sale;
+    });
+  
+  if (resultProducts.length !== arraySales.length) return false;
+
+  return true;
+};
+
+const existsQuantityOfProducts = async (arraySales) => {
+  const products = await ProductsModel.getAll();
+  const salesDetails = await arraySales
+  .filter((sale) => {
+    const product = products.find((p) => p.id === sale.productId);
+
+    if (sale.quantity > product.quantity) return false;
+
+    return sale;
+  });
+
+  if (salesDetails.find((sale) => sale === false)) return false;
+
+  return salesDetails;
+};
+
+const findSale = async (id, productId) => {
+  const sales = await SalesModel.find(Number(id));
+
+  return sales.find((sale) => sale.productId === productId);
+};
 
 const getAll = async () => {
   const result = await SalesModel.getAll();
@@ -11,41 +55,70 @@ const find = async (id) => {
 };
 
 const create = async (arraySales) => {
-  // verificar se há quantidade suficiente de produtos.
+  const result = await existsQuantityOfProducts(arraySales);
 
-  const result = await SalesModel.create(arraySales);
+  if (result.length === 0) return undefined;
 
-  if (result.length === 0) return [];
+  const resultSales = await SalesModel.create(arraySales);
+  if (resultSales.length === 0) return [];
 
-  // fazer update da quantidade dos produtos.
+  // em caso de erro abaixo, tem que fazer o rollback acima.
+  // Não implementado.
+  if (updateQuantityProducts(arraySales) === false) return false;
 
-  return result;
+  return resultSales;
 };
 
 const update = async (sale) => {
-  // verificar se há quantidade suficiente de produtos.
+  // verificar se a quantidade a ser atualizada é a mesma da venda,
+  // não fazer nada. Não implementado.
+  const saleDetails = await findSale(sale.id, sale.productId);
+  if (saleDetails.length === 0) return [];
+
+  const difference = saleDetails.quantity - sale.quantity;
+
+  if (difference < 0 && !existsQuantityOfProducts([{
+      productId: sale.productId,
+      quantity: Math.abs(difference),
+    }])) return undefined;
 
   const result = await SalesModel
     .update({
       saleId: Number(sale.id),
-      productId: Number(sale.productId),
-      quantity: Number(sale.quantity),
+      productId: sale.productId,
+      quantity: sale.quantity,
     });
 
-  if (result.affectedRows === 0) return false;
-
-  // fazer update da quantidade dos produtos.
+  // em caso de erro abaixo, tem que fazer o rollback acima.
+  // Não implementado.
+  if (updateQuantityProducts([{
+    productId: sale.productId,
+    quantity: (difference * -1),
+  }]) === false) return false;
 
   return result;
 };
 
 const exclude = async (id) => {
-  const sale = await SalesModel.find(Number(id));
-  console.log(sale);
-  if (sale.length === 0) return false;
+  // ao excluir uma venda, atualiza a quantidade em produtos.
+  const sales = await SalesModel.find(Number(id));
 
-  const result = await SalesModel.exclude(id);
+  if (sales.length === 0) return undefined;
 
+  const updateArraySales = sales.map((sale) => {
+    const product = {
+      date: sale.date,
+      productId: sale.productId,
+      quantity: (sale.quantity * -1),
+    };
+    return product;
+  });
+
+  if (updateQuantityProducts(updateArraySales) === false) return [];
+  
+  // em caso de erro abaixo, tem que fazer o rollback acima.
+  // Não implementado.
+  const result = await SalesModel.exclude(Number(id));
   if (!result) return false;
 
   return result;
